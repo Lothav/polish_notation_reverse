8fc8b2864286702adef08dbd12174666fbe908b8
Luiz Otavio <luizotrv@gmail.com>
2017-04-14 23:15:09 -0300

    empty caracter

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define EMPTY_CHAR 'E'
 #define OPERATION_MISSING '?'
 
 typedef struct{
@@ -30,21 +31,47 @@ int main(int argc, char** argv) {
     fb = fopen(argv[1], "r");
     if ( NULL == fb ) exit(EXIT_FAILURE);
 
-    int i = 0;
+    int i = 0, j = 0, k = 0;
     while ( -1 != (line_size = getline(&line, &len, fb))){
+
         while( NULL != (value = strtok(NULL == value ? line : NULL, " \n")) ){
 
+            if( EMPTY_CHAR  == *value ) continue;
+
             if( OPERATION_MISSING  == *value ) {
+
+                while(k < line_size){
+                    if(line[k] == '\0'){
+                        line[k] = ' ';
+                        j = i;
+                    }
+                    k++;
+                }
+
                 operations = realloc(operations, (1+actual_ope) * sizeof(Operation));
                 operations[actual_ope].a = operators[0];
                 operations[actual_ope].b = operators[1];
                 operations[actual_ope].op_a = NULL;
                 operations[actual_ope].op_b = NULL;
 
+                line[j] = 'G';
+
+                j = 3;
+                i--;
+                while( j && i >= 0 ){
+                    if(line[i] == ' '){
+                        j--;
+                        i--;
+                        continue;
+                    }
+                    line[i] = 'E';
+                    i--;
+                }
                 i = 0;
+                value = NULL;
             } else {
                 operators[i % 2] = atoi( value );
-                i++;
+                i += strlen(value)+1;
             }
         }
     }
