b812f5545e2943916261056b79a42f21ab2f734b
Luiz Otavio <luizotrv@gmail.com>
2017-04-21 17:30:52 -0300

    utils comments

--- utils.h
+++ utils.h
@@ -1,7 +1,14 @@
 #ifndef POLISH_NOTATION_REVERSE_UTILS_H
 #define POLISH_NOTATION_REVERSE_UTILS_H
 
+/*
+ * convert a decimal number num to a sequence of operators
+ * ex.: num = 5
+ * binary of 5 = 101
+ * so, str will be set as "+*+"
+ * (0's are set as '+' and 1's are set as *)
+ *
+*/
 void convertNumberToOperator(int num, char *str, int op_count);
-
 
 #endif //POLISH_NOTATION_REVERSE_UTILS_H
